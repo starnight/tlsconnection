e42c662922872a382b335a3d559045c3b06fe22c
Jian-Hong Pan <starnight@g.ncu.edu.tw>
2019-09-05 23:33:04 +0800

    Change from sync to async process

--- client.py
+++ client.py
@@ -1,17 +1,20 @@
 #!/bin/env python3
 
-import socket
+import asyncio
 import ssl
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 context.load_verify_locations("server.crt")
 context.check_hostname = False
 
-cli_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssock = context.wrap_socket(cli_sock)
-ssock.connect(('127.0.0.1', 8443))
+async def client_process(loop):
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8443, ssl=context, loop=loop)
+    writer.write("test".encode())
+    data = await reader.read(256)
+    print(data.decode())
+    writer.close()
 
-ssock.send("test".encode())
-data = ssock.recv(256)
-print(data.decode())
-ssock.close()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(client_process(loop))
+    loop.close()
--- server.py
+++ server.py
@@ -1,36 +1,34 @@
 #!/bin/env python3
 
-import socket
+import asyncio
 import ssl
 
 class server:
     def __init__(self):
+        self.svr_ip = "127.0.0.1"
+        self.svr_port = 8443
         self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         self.context.load_cert_chain("server.crt", "server.key")
-        self.svr_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def deal_cli(self, ssock):
-        data = ssock.recv(256)
-        print("Client {}: {}".format(ssock.getpeername(), data.decode()))
+    async def deal_cli(self, reader, writer):
+        addr = writer.get_extra_info('peername')
+        data = await reader.read(256)
+        print("Client {}: {}".format(addr, data.decode()))
         data = "Server got {}".format(data.decode())
-        ssock.send(data.encode())
+        writer.write(data.encode())
+        writer.close()
 
     def run(self):
-        self.svr_sock.bind(('127.0.0.1', 8443))
-        self.svr_sock.listen(5)
+        self.loop = asyncio.get_event_loop()
+        coro = asyncio.start_server(self.deal_cli, self.svr_ip, self.svr_port, ssl=self.context, loop=self.loop)
+        self.loop.run_until_complete(coro)
 
-        while True:
-            cli_sock, addr = self.svr_sock.accept()
-            ssock = self.context.wrap_socket(cli_sock, server_side=True)
-
-            try:
-                self.deal_cli(ssock)
-            finally:
-                ssock.shutdown(socket.SHUT_RDWR)
-                ssock.close()
-
-    def __del__(self):
-        self.svr_sock.close()
+        try:
+            self.loop.run_forever()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.loop.close()
 
 if __name__ == '__main__':
     svr = server()
